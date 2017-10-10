b0071d58743ef89a029fcda80a06e25a908143af
lilia <liliakai@gmail.com>
2017-10-10 11:22:05 +0200

    Refactor

--- graph.js
+++ graph.js
@@ -55,124 +55,156 @@ var simulation = d3.forceSimulation()
     .force("balance", d3.forceX(0).strength(0.01))
     .alphaDecay(0);
 
-function test(url) {
+var maxAdded = 0;
+function handleJSON(json) {
   var nodes = simulation.nodes();
   var links = simulation.force("link").links();
+  var added = 0;
 
-  $.getJSON(url, function(requests) {
-    $.each(requests, function(name, request){
-      var ssid = ssids[request.name];
-      if (!ssid) {
-        console.log('adding ssid', request.name);
-        ssid = {
-          name: request.name,
-          lastSeen: request.lastSeen,
-          count: request.count,
-          macs: []
-        };
-        nodes.push(ssid);
-        ssids[ssid.name] = ssid;
+  $.each(json, function(name, request){
+    if (maxAdded && added > maxAdded) {
+      return false;
+    }
+    var ssid = ssids[request.name];
+    if (!ssid) {
+      added++;
+      console.log('adding ssid', request.name);
+      ssid = {
+        name: request.name,
+        lastSeen: request.lastSeen,
+        count: request.count,
+        macs: {}
+      };
+      nodes.push(ssid);
+      ssids[ssid.name] = ssid;
+    } else {
+      // update stats
+      ssid.lastSeen = request.lastSeen;
+      ssid.count = request.count;
+    }
+    $.each(request.macs, function(address, count) {
+      var mac = macs[address];
+      if (!mac) {
+        console.log('adding mac', address);
+        mac = { address: address, count: count };
+        nodes.push(mac);
+        macs[address] = mac;
       } else {
-        // update stats
-        ssid.lastSeen = request.lastSeen;
-        ssid.count = request.count;
+        mac.count = count;
       }
-      $.each(request.macs, function(address, count) {
-        var mac = macs[address];
-        if (!mac) {
-          console.log('adding mac', address);
-          mac = { address: address, count: count };
-          nodes.push(mac);
-          macs[address] = mac;
-        } else {
-          mac.count = count;
-        }
-        if (!ssid.macs[address]) {
-          console.log('adding link', mac.address, ssid.name);
-          links.push({ source: mac, target: ssid });
-        }
-        ssid.macs[address] = count;
-      });
+      if (!ssid.macs[address]) {
+        console.log('adding link', mac.address, ssid.name);
+        links.push({ source: mac, target: ssid });
+      }
+      ssid.macs[address] = count;
     });
-    simulation.nodes(nodes);
-    simulation.force("link").links(links);
+    ssid.numMacs = d3.keys(ssid.macs).length;
+  });
+  simulation.nodes(nodes);
+  simulation.force("link").links(links);
 
-    var line = g.selectAll(".line").data(links);
-    var lineEnter = line.enter().append("line").attr("class", "line")
-      .attr("x1", function(d) { return d.source.x })
-      .attr("y1", function(d) { return d.source.y })
-      .attr("x2", function(d) { return d.target.x })
-      .attr("y2", function(d) { return d.target.y })
-      .style("stroke", "rgb(6,120,155)");
+  var line = g.selectAll(".line").data(links);
+  var lineEnter = line.enter().append("line").attr("class", "line")
+    .attr("x1", function(d) { return d.source.x })
+    .attr("y1", function(d) { return d.source.y })
+    .attr("x2", function(d) { return d.target.x })
+    .attr("y2", function(d) { return d.target.y })
+    .style("stroke", "rgb(6,120,155)");
 
-    var mac = g.selectAll(".mac").data(d3.values(macs));
-    var macEnter = mac.enter().append("g").attr("class", "mac");
-    var macInner = macEnter.append("g").attr("transform", "translate(-" + rectWidth/2 + ",-" + rectWidth/2 + ")");
-    macInner.append("svg:rect")
-      .attr("width", rectWidth + 2)
-      .attr("height", rectWidth + 2)
-      .attr("x", -1)
-      .attr("y", -1)
-      .attr("fill", "#666666")
-    macInner.append("svg:polygon")
-      .attr("points", function(d) {
-        return [
-          [0,0], [0,rectWidth], [rectWidth,0]
-        ].map(function(p) { return p.toString(); }).join(' ');
-      })
-      .attr("fill", function(d, i) {
-        var octets = d.address.split(':');
-        var color = '#' + octets.slice(0,3).join('');
-        return color;
-      });
-    macInner.append("svg:polygon")
-      .attr("points", function(d) {
-        return [
-          [rectWidth,0], [rectWidth,rectWidth], [0, rectWidth]
-        ].map(function(p) { return p.toString(); }).join(' ');
-      })
-      .attr("fill", function(d, i) {
-        var octets = d.address.split(':');
-        var color = '#' + octets.slice(3).join('');
-        return color;
-      });
-
-    var ssid = g.selectAll(".ssid").data(d3.values(ssids));
-    var ssidEnter = ssid.enter().append("g").attr("class", "ssid");
-    //CIRCLE
-    ssidEnter.append("svg:circle")
-      .attr("cx", function(d) { return 0; })
-      .attr("cy", function(d) { return 0; })
-      .attr("r", function(d) { return circleWidth; })
-      .attr("fill", function(d, i) { return  palette.pink; });
-    //TEXT
-    ssidEnter.append("text")
-      .text(                function(d, i) { return d.name; })
-      .attr("x",            function(d, i) { return 10; })
-      .attr("y",            function(d, i) { return  5; })
-      .attr("fill",         function(d, i) {  return  palette.paleryellow;  })
-      .attr("font-size",    function(d, i) {  return  "1em"; })
-      .attr("font-family",  "Bree Serif")
-
-    simulation.on("tick", function(e, alpha) {
-       macEnter.merge(mac).attr("transform", function(d, i) {
-          return "translate(" + d.x + "," + d.y + ")";
-       });
-       ssidEnter.merge(ssid).attr("transform", function(d, i) {
-          return "translate(" + d.x + "," + d.y + ")";
-       });
-
-      lineEnter.merge(line).attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; })
-
+  var mac = g.selectAll(".mac").data(d3.values(macs));
+  var macEnter = mac.enter().append("g").attr("class", "mac");
+  var macInner = macEnter.append("g").attr("transform", "translate(-" + rectWidth/2 + ",-" + rectWidth/2 + ")");
+  macInner.append("svg:rect")
+    .attr("width", rectWidth + 2)
+    .attr("height", rectWidth + 2)
+    .attr("x", -1)
+    .attr("y", -1)
+    .attr("fill", "#666666")
+  macInner.append("svg:polygon")
+    .attr("points", function(d) {
+      return [
+        [0,0], [0,rectWidth], [rectWidth,0]
+      ].map(function(p) { return p.toString(); }).join(' ');
+    })
+    .attr("fill", function(d, i) {
+      var octets = d.address.split(':');
+      var color = '#' + octets.slice(0,3).join('');
+      return color;
+    });
+  macInner.append("svg:polygon")
+    .attr("points", function(d) {
+      return [
+        [rectWidth,0], [rectWidth,rectWidth], [0, rectWidth]
+      ].map(function(p) { return p.toString(); }).join(' ');
+    })
+    .attr("fill", function(d, i) {
+      var octets = d.address.split(':');
+      var color = '#' + octets.slice(3).join('');
+      return color;
     });
 
-    setTimeout(function() { test(url); }, 2000);
+  var ssid = g.selectAll(".ssid").data(d3.values(ssids));
+  var ssidEnter = ssid.enter().append("g").attr("class", "ssid");
+  //CIRCLE
+  ssidEnter.append("svg:circle")
+    .attr("cx", function(d) { return 0; })
+    .attr("cy", function(d) { return 0; })
+    .attr("r", function(d) { return circleWidth; })
+    .attr("fill", function(d, i) { return  palette.pink; });
+  //TEXT
+  ssidEnter.append("text")
+    .text(                function(d, i) { return d.name; })
+    .attr("x",            function(d, i) { return 10; })
+    .attr("y",            function(d, i) { return  5; })
+    .attr("fill",         function(d, i) {  return  palette.paleryellow;  })
+    .attr("font-size",    function(d, i) {  return  "1em"; })
+    .attr("font-family",  "Bree Serif")
+
+  simulation.on("tick", function(e, alpha) {
+      macEnter.merge(mac).attr("transform", function(d, i) {
+        return "translate(" + d.x + "," + d.y + ")";
+      });
+      ssidEnter.merge(ssid).attr("transform", function(d, i) {
+        return "translate(" + d.x + "," + d.y + ")";
+      });
+
+    lineEnter.merge(line).attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; })
+
   });
 }
 
-test('probereq.json');
-//test('probereq-test.json');
+poll('probereq.json');
+//poll('probereq-test.json');
 
+function poll(url) {
+  $.getJSON(url, function(json) {
+    handleJSON(json);
+    setTimeout(function() { poll(url); }, 2000);
+  });
+}
+
+function test(url) {
+  $.getJSON(url, function(testRequests) {
+    var names = Object.keys(testRequests);
+    run();
+
+    function run() {
+        if (Math.random() < 0.1) {
+          var randomName = names[Math.floor(Math.random() * names.length/2)];
+          increment(testRequests[randomName]);
+        }
+
+        handleJSON(testRequests);
+        setTimeout(run, 1000);
+    };
+
+    function increment(request) {
+        request.lastSeen = Date.now() / 1000;
+        request.count += 1;
+        console.log('simulating probe request', request.name);
+    }
+  });
+}
